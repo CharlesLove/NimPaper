5eb120e087dae644ffa554beea736d9bbfe3cb3f
Chaz <chazlouviere@gmail.com>
2021-07-05 18:08:38 -0500

    first commit

---
+++ nimpaper.nim
@@ -0,0 +1,99 @@
+import smtp, rss, strutils, FeedNim, times, parseopt
+
+var
+  eml = ""
+  pwd = ""
+  configFile = ""
+
+for kind,key,val in getOpt():
+  case kind
+    of cmdArgument:
+      echo("Got a command argument: ", key)
+      configFile = $key
+    of cmdLongOption,cmdShortOption:
+      case key
+      of "port": echo("Got port: ", val)
+      else: echo("Got another flag --",key," with value: ",val)
+    of cmdEnd: discard
+
+var fullMsg = "<html><body>"
+
+proc sendMail(fromAddr: string; toAddrs, ccAddrs: seq[string];
+              subject, message, login, password: string;
+              server = "smtp.gmail.com"; port = Port 465; ssl = true) =
+  let msg = createMessage(subject, message, toAddrs, ccAddrs, [("Content-Type", "text/html"), ("charset","UTF-8"),("Content-Transfer-Encoding", "quoted-printable")])
+  let session = newSmtp(useSsl = ssl, debug = true)
+  session.connect(server, port)
+  session.auth(login, password)
+  session.sendMail(fromAddr, toAddrs, $msg)
+
+proc buildFeed(feedUrl: string,itemCount: int, fullText = false , isAtom = false): string =
+  var formattedMsg = ""
+  
+  if not isAtom:
+    let rssFeed = rss.getRSS(feedUrl)
+    formattedMsg = formattedMsg & "<h2><a href=3D'" & rssFeed.link & "'>" & rssFeed.title & "</a></h2><ul>"
+
+    for i in 0 ..< itemCount:
+      formattedMsg = formattedMsg & "<li><h3><a href=3D'" & rssFeed.items[i].link & "'>" & rssFeed.items[i].title & "</a></h3>"
+
+      if fullText:
+        formattedMsg = formattedMsg & "<p>" & rssFeed.items[i].description.replace("href=", "href=3D").replace("<hr />","") & "</p></li>"
+      else:
+        formattedMsg = formattedMsg & "</li>"
+
+    formattedMsg = formattedMsg & "</ul>"
+
+  else:
+    let atomFeed = getAtom(feedUrl)
+    formattedMsg = formattedMsg & "<h2><a href=3D'" & atomFeed.link.href & "'>" & atomFeed.title.text & "</a></h2><ul>"
+
+    for i in 0 ..< itemCount:
+      formattedMsg = formattedMsg & "<li><h3><a href=3D'" & atomFeed.entries[i].link.href & "'>" & atomFeed.entries[i].title.text & "</a></h3>"
+
+      if fullText:
+        formattedMsg = formattedMsg & "<p>" & atomFeed.entries[i].content.text.replace("href=", "href=3D").replace("<hr />","") & "</p></li>"
+      else:
+        formattedMsg = formattedMsg & "</li>"
+
+    formattedMsg = formattedMsg & "</ul>"
+
+
+  
+  return formattedMsg
+
+proc parseCfg() =
+  var
+    mode = ""
+  let input = open(configFile)
+  for line in input.lines:
+    case mode:
+      of "[login]":
+        if(line.startsWith("email:")):
+          eml = line.split('"')[1]
+        elif(line.startsWith("password:")):
+          pwd = line.split('"')[1]
+        else:
+          mode = line
+      of "[feeds]":
+        if(line.startsWith("title:")):
+          fullMsg = fullMsg & "<h1>" & line.split('"')[1] & "</h1>"
+        else:
+          var splLine = line.split(',')
+
+          fullMsg = fullMsg & buildFeed($splLine[0].replace("\"",""),splLine[1].parseInt(),splLine[2].parseBool,splLine[3].parseBool)
+            
+      else:
+        mode = line
+
+parseCfg()
+
+fullMsg = fullMsg & "</body></html>"
+
+sendMail(fromAddr = eml,
+        toAddrs  = @[eml],
+        ccAddrs  = @[],
+        subject  = "NimBrew - " & $now().weekday & " " & $now().format("MM-dd-yyyy htt"),
+        message  = fullMsg,
+        login    = eml,
+        password = pwd)
