369e32c7ca67bdbdd39eda169b70641fd66e4fcc
Chaz <chazlouviere@gmail.com>
2021-08-27 15:02:08 -0500

    Atom feeds remove posts from over 24 hours. Both feed types hide feeds that are empty.

--- nimpaper.nim
+++ nimpaper.nim
@@ -14,10 +14,16 @@ for kind,key,val in getOpt():
 
 var fullMsg = "<html><body>"
 
-#var modPubDate = "Thu, 26 Aug 2021 15:00:00 +0000";
+#var modPubDate = "Thu, 26 Aug 2021 15:00:00 +0000"
 #modPubDate = modPubDate[0 .. ^3] & ":" & modPubDate[^2 .. ^1]
 #doAssert((parse(modPubDate, "ddd, dd MMM yyyy HH:mm:ss zzz").utc - now().utc).inHours > 24)
 #echo (now().utc - parse(modPubDate, "ddd, dd MMM yyyy HH:mm:ss zzz").utc).inHours
+
+#var modPublished = "2021-08-27T02:08:26+00:00"
+#modPublished = modPublished[0 .. 9] & " " & modPublished[11 .. ^1]
+#modPublished.insert(" ", 19)
+#echo(modPublished)
+#echo (now().utc - parse(modPublished, "yyyy-MM-dd HH:mm:ss zzz").utc).inHours
 #quit()
 
 proc sendMail(fromAddr: string; toAddrs, ccAddrs: seq[string];
@@ -34,7 +40,9 @@ proc buildFeed(feedUrl: string,itemCount: int, fullText = false , isAtom = false
   try:
     if not isAtom:
       let rssFeed = rss.getRSS(feedUrl)
-      formattedMsg = formattedMsg & "<h2><a href=3D'" & rssFeed.link & "'>" & rssFeed.title & "</a></h2><ul>"
+
+      var feedMsg = ""
+      var totalStories = 0
 
       for i in 0 ..< itemCount:
         # Sometimes the amount of news is smaller than what the user sets
@@ -44,35 +52,57 @@ proc buildFeed(feedUrl: string,itemCount: int, fullText = false , isAtom = false
           modPubDate = modPubDate[0 .. ^3] & ":" & modPubDate[^2 .. ^1]
           if (now().utc - parse(modPubDate, "ddd, dd MMM yyyy HH:mm:ss zzz").utc).inHours > 24:
             continue
-          formattedMsg = formattedMsg & "<li><h3><a href=3D'" & rssFeed.items[i].link & "'>" & rssFeed.items[i].title & "</a></h3>"
+          feedMsg = feedMsg & "<li><h3><a href=3D'" & rssFeed.items[i].link & "'>" & rssFeed.items[i].title & "</a></h3>"
         except:
           break
         
         #formattedMsg = formattedMsg & "<p>" & rssFeed.items[i].pubDate & "</p>"
 
         if fullText:
-          formattedMsg = formattedMsg & "<p>" & rssFeed.items[i].description.replace("href=", "href=3D").replace("<hr />","") & "</p></li>"
+          feedMsg = feedMsg & "<p>" & rssFeed.items[i].description.replace("href=", "href=3D").replace("<hr />","") & "</p></li>"
         else:
-          formattedMsg = formattedMsg & "</li>"
+          feedMsg = feedMsg & "</li>"
+        
+        totalStories += 1
 
-      formattedMsg = formattedMsg & "</ul>"
+      feedMsg = feedMsg & "</ul>"
+
+      if(totalStories > 0):
+        formattedMsg = formattedMsg & "<h2><a href=3D'" & rssFeed.link & "'>" & rssFeed.title & "</a></h2><ul>" & feedMsg
 
     else:
       let atomFeed = getAtom(feedUrl)
-      formattedMsg = formattedMsg & "<h2><a href=3D'" & atomFeed.link.href & "'>" & atomFeed.title.text & "</a></h2><ul>"
 
+      var feedMsg = ""
+
+      var totalStories = 0
       for i in 0 ..< itemCount:
         # Sometimes the amount of news is smaller than what the user sets
         try:
-          formattedMsg = formattedMsg & "<li><h3><a href=3D'" & atomFeed.entries[i].link.href & "'>" & atomFeed.entries[i].title.text & "</a></h3>"
+          #"2021-08-27T02:08:26+00:00"
+          var modPublished = atomFeed.entries[i].published
+          modPublished = modPublished[0 .. 9] & " " & modPublished[11 .. ^1]
+          modPublished.insert(" ", 19)
+          if (now().utc - parse(modPublished, "yyyy-MM-dd HH:mm:ss zzz").utc).inHours > 24:
+            continue
+
+          feedMsg = feedMsg & "<li><h3><a href=3D'" & atomFeed.entries[i].link.href & "'>" & atomFeed.entries[i].title.text & "</a></h3>"
         except:
           break
-        if fullText:
-          formattedMsg = formattedMsg & "<p>" & atomFeed.entries[i].content.text.replace("href=", "href=3D").replace("<hr />","") & "</p></li>"
-        else:
-          formattedMsg = formattedMsg & "</li>"
 
-      formattedMsg = formattedMsg & "</ul>"
+        #feedMsg = feedMsg & "<p>" & atomFeed.entries[i].published & "</p>"
+
+        if fullText:
+          feedMsg = feedMsg & "<p>" & atomFeed.entries[i].content.text.replace("href=", "href=3D").replace("<hr />","") & "</p></li>"
+        else:
+          feedMsg = feedMsg & "</li>"
+
+        totalStories += 1
+
+      feedMsg = feedMsg & "</ul>"
+
+      if(totalStories > 0):
+        formattedMsg = formattedMsg & "<h2><a href=3D'" & atomFeed.link.href & "'>" & atomFeed.title.text & "</a></h2><ul>" & feedMsg
 
   except:
     formattedMsg = "<br>Error with: " & feedUrl
